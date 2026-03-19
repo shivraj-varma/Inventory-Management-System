8137aa10678e6ae8ac4b7cf00b33724911d0da12
shivraj-varma <shivrajvarma07@gmail.com>
2026-03-19 11:04:04 +0530

    Project under construction

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
 import mysql.connector              # cannect sql database 
 from mysql.connector import Error   
-import pandas as pd                 
+import pandas as pd  
+import csv               
 
-
-class sqL_connection:
-    pass
-
-class Product:
-    pass
-
-class Inventory:
-    pass
-
-class Sales:
-    pass
-
-class Suppliers:
-    pass
